7aab87f1afe33fe3589f5a0dedfce5494f82e37e
Ale E <alegescalante@gmail.com>
2025-05-05 15:47:09 -0300

    primer

---
+++ main.tf
@@ -0,0 +1,16 @@
+
+terraform {
+  required_version = ">= 0.12"
+}
+
+####### provider #######
+provider "aws" {
+  region = "us-east-1"
+  profile = "tomate"
+}
+
+####### resource #######
+resource "aws_instance" "nginex-server" {
+  ami = "aws-0f88e80871fd81e91" 
+  instance_type = "t3.micro"
+}
